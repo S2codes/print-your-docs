051a467d65d20436b4b44139578125beb2a8a023
S2codes <25subhankar@gmail.com>
2022-10-22 15:34:41 +0530

    responsive complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,8 @@ $(document).ready(function () {
     }
 
     // sticky Nav 
-    console.log('qry');
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        console.log(scroll);
         if (scroll > 20) {
             $("#navbar").addClass("sticky");
         } else {
@@ -17,4 +15,67 @@ $(document).ready(function () {
         }
     });
 
+    function appendFileData(data) {
+        $('#documentList').append(`
+        <div class="item">
+        <p class="m-0"><strong>FileName :</strong> ${data[0]}</p>
+        <p class="m-0">${data[1]} x ${data[2]} x ${data[3]}</p>
+    </div>`);
+    }
+
+
 })
+
+
+// upload documents  
+document.getElementById('uploadDForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    console.log('from submit');
+    const userFile = document.getElementById("file").files[0];
+    console.log('file data');
+    console.log(userFile);
+    console.log(userFile.name);
+
+    const color = document.getElementById("colorType").value;
+    const paper = document.getElementById("paperType").value;
+    const num_copies = document.getElementById("copiesNo").value;
+
+    let Filedata = [userFile.name, color, paper, num_copies];
+
+    const formData = new FormData();
+
+    formData.append('user-File', userFile);
+    formData.append('color', color)
+    formData.append('paper', paper)
+    formData.append('number_copies', num_copies)
+    formData.append('id', 1245)
+
+    console.log(formData.values());
+
+    fetch('http://localhost/fileuploader/api/post.php', {
+        method: "POST",
+        body: formData
+    })
+        .then(res => res.json())
+        .then(data => appendFileData(Filedata))
+        .catch(err => console.log(err))
+
+
+})
+
+
+function appendFileData(data) {
+    // document.getElementById('documentList').appendChild(`
+    //     <div class="item">
+    //         <p class="m-0"><strong>FileName :</strong> ${data[0]}</p>
+    //         <p class="m-0">${data[1]} x ${data[2]} x ${data[3]}</p>
+    //     </div>
+    // `);
+
+    document.getElementById('documentList').innerHTML += `  <div class="item">
+    <p class="m-0"><strong>FileName :</strong> ${data[0]}</p>
+    <p class="m-0">${data[1]} x ${data[2]} x ${data[3]}</p>
+</div>`;
+
+}
